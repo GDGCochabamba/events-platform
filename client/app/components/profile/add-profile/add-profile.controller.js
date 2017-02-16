ee1787c6f6293496c899c1924d5e3e8885aa1f89
MartinArispeRiveros <martinarisperiveros@gmail.com>
2017-02-16 00:10:08 -0400

    Added relationship between user and profiles

--- client/app/components/profile/add-profile/add-profile.controller.js
+++ client/app/components/profile/add-profile/add-profile.controller.js
@@ -4,16 +4,22 @@ function AddProfileController($log, ProfileService) {
   ctrl.$onInit  = onInit;
   ctrl.add = add;
 
-  function add() {
-    ProfileService.add(ctrl.profile).then(function(ref){
+  function add() {    
+    ProfileService.add(ctrl.profile, ctrl.user).then(function(ref){
       var id = ref.key;
       $log.info('[AddProfileController]', 'added record with id:', id);
     });
   }
+  
+  
 
   function onInit() {
     ctrl.profile = {
       firstName: 'Martin'
+    };
+
+    ctrl.user = {
+      email: ''
     };
   }
 }
--- client/app/components/profile/profile.service.js
+++ client/app/components/profile/profile.service.js
@@ -1,23 +1,37 @@
-function ProfileService($log, $firebaseArray, $q) {
+function ProfileService($log, $firebaseArray, $firebaseObject, $firebaseAuth, $q) {
   var ref = firebase.database().ref().child('profiles'),
       list = $firebaseArray(ref);
+
       service = {
-          add: add
+          add: add,
+          createProfile: createProfile
       };
 
       return service;
 
-      function add(profile) {
-        $log.info('[ProfielService]', 'add profile', profile);
+     function add(profile, user) {
         var deferred = $q.defer();
-        list.$add(profile).then(function(ref) {
-            deferred.resolve(ref);
-        }, function(error){
-            deferred.reject(error);
+        var user = $firebaseAuth().$createUserWithEmailAndPassword(user.email, user.password).then(function (authData) {
+            console.log(authData.uid); //should log new uid.
+            var fprofile = $firebaseObject(ref.child(authData.uid));
+            fprofile.firstName = profile.firstName;
+            fprofile.lastName = profile.lastName;
+            fprofile.birthDate = profile.birthDate;
+            fprofile.phone = profile.phone;
+            fprofile.gender = profile.gender;
+            fprofile.$save().then(function (result) {
+                deferred.resolve(result);
+            }).catch(function (err) {
+                deferred.reject(profile);
+            });
         });
-
         return deferred.promise;
-      }
+    }
+
+      function createProfile(user, authData){        
+        var profileRef = $firebase(ref.child('profile'));
+        return profileRef.$set(authData.uid, user);
+      };
 }
 
 angular
