cee0bd2ac452ac249bb752f4a57f758fd66e0653
Julio Silva <julio.sguz@gmail.com>
2017-02-20 08:38:31 -0400

    Remove attendees component on module

--- client/app/components/components.module.js
+++ client/app/components/components.module.js
@@ -2,11 +2,9 @@ angular
   .module('components', [
     'components.auth',
     'components.event',
-    'components.attendees',
     'components.profile'
   ]);
 
 require('./auth/auth.module');
-require('./attendees/attendees.module');
 require('./event/event.module');
 require('./profile/profile.module');
