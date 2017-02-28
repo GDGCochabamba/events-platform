3f75011337d6ed5006e792ca58cf2fd9cd0f2bfb
MartinArispeRiveros <martinarisperiveros@gmail.com>
2017-02-28 01:36:25 -0400

    Adding link from profile list to view profile for each profile of the list

--- client/app/components/profile/view-profile/view-profile.controller.js
+++ client/app/components/profile/view-profile/view-profile.controller.js
@@ -3,13 +3,6 @@ function ViewProfileController($log, $state, $stateParams, ProfileService) {
 
   ctrl.$onInit  = onInit;
   // ctrl.add = add;
-
-  // function add() {    
-  //   ProfileService.add(ctrl.profile, ctrl.user).then(function(ref){
-  //     var id = ref.key;
-  //     $log.info('[AddProfileController]', 'added record with id:', id);
-  //   });
-  // }
   
   
   function onInit() {
@@ -19,15 +12,7 @@ function ViewProfileController($log, $state, $stateParams, ProfileService) {
         ctrl.profile = profile;
       });
   }
-  // function onInit() {
-  //   ctrl.profile = {
-  //     firstName: 'Martin'
-  //   };
-
-  //   ctrl.user = {
-  //     email: ''
-  //   };
-  // }
+  
 }
 
 angular
